0b80f93ee066df8d43648c6d9c218fcfb9aaad11
Janella-codes <118861074+Janella-codes@users.noreply.github.com>
2023-10-11 12:50:16 -0700

    Router

--- apps/nextjs/src/pages/components/SideNav.tsx
+++ apps/nextjs/src/pages/components/SideNav.tsx
@@ -1,13 +1,10 @@
-import {
-  SignInButton,
-  SignOutButton,
-  useSession,
-} from "@clerk/nextjs/dist/client";
+import { SignInButton, SignOutButton, useSession } from "@clerk/nextjs";
 import Link from "next/link";
+import { trpc } from "../../utils/trpc";
 
 export function SideNav() {
   const session = useSession();
-  const user = session.data?.user;
+  const user = trpc.post.all.useQuery();
 
   return (
     <nav className="sticky top-0 px-2 py-4">
@@ -87,7 +84,7 @@ export function SideNav() {
                   d="M11.02 3.77v1.56l1-.99V2.5l-.5-.5h-9l-.5.5v.486L2 3v10.29l.36.46 5 1.72L8 15v-1h3.52l.5-.5v-1.81l-1-1V13H8V4.71l-.33-.46L4.036 3h6.984v.77zM7 14.28l-4-1.34V3.72l4 1.34v9.22zm6.52-5.8H8.55v-1h4.93l-1.6-1.6.71-.7 2.47 2.46v.71l-2.49 2.48-.7-.7 1.65-1.65z"
                 />
               </svg>
-              <span className="hidden text-lg text-red-700 md:inline">
+              <span className="hidden text-lg text-green-700 md:inline">
                 Log out
               </span>
             </span>
